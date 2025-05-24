8785a21f79eb635937a3250d71820fe1d1f7bea3
ant-rice <antonio_rice@icloud.com>
2025-05-24 01:10:09 -0700

    finished fixhtml method need to fix tostring
    tests 1,2,3 work
    tests 4,5 do not work

--- HTMLManager.java
+++ HTMLManager.java
@@ -24,7 +24,34 @@ public class HTMLManager {
    }
    
    public void fixHTML() {
-      Stack<HTMLTag> fixThis = new Stack<>();
-      // add fix logic here
+      Stack<HTMLTag> openTags = new Stack<>();
+      Queue<HTMLTag> fixed = new LinkedList<>();
+      
+      while(!tags.isEmpty()) {
+         HTMLTag current = tags.poll();
+         if(current.isSelfClosing()) {
+            fixed.add(current);
+         }
+         else if(current.isOpening()) {
+            openTags.push(current);
+            fixed.add(current);
+            
+         }
+         else if(current.isClosing()) {
+            if(!openTags.isEmpty() && openTags.peek().matches(current)) {
+               fixed.add(current);
+               openTags.pop();
+            }
+            else if(!openTags.isEmpty() && !openTags.peek().matches(current)) {
+               fixed.add(openTags.pop().getMatching());
+            }  
+         }
+      }
+      
+      while (!openTags.isEmpty()) {
+         fixed.add(openTags.pop().getMatching());
+      }
+         
+      tags = fixed;
    }
 }
